701261ee06214cf80431a7bcbada30d1d040a8cc
Phillip Alday <me@phillipalday.com>
2022-08-04 02:44:28 -0500

    loopty loop

--- src/FiveLetterWorda.jl
+++ src/FiveLetterWorda.jl
@@ -3,6 +3,7 @@ module FiveLetterWorda
 using Arrow
 using Combinatorics
 using Downloads
+using ProgressMeter
 using Scratch
 using ZipFile
 
@@ -85,28 +86,74 @@ remove_anagrams(words::Vector{String}) = unique(Set, words)
 
 function main()
     words = remove_anagrams(load_data())
-    # wcs = WordCombination.(combinations(wl, 2))
-    find_word_combo(WordCombination(), words)
+    words = filter(x -> length(Set(x)) == 5, words)
+    hits = sizehint!(Set{WordCombination}(), 538)
+    find_word_combo!(hits, words)
+    return hits
 end
 
-function find_word_combo(wc::WordCombination, wordlist::Vector{String}, idx::Int=1)
-    word = wordlist[idx]
-    # word in wc && error("got a repeat! $(wc), $(idx)")
+function skip(wc::WordCombination, word::String)
+    return word in wc || length(setdiff(word, wc.chars)) != 5
+end
 
-    wc_new = union(wc, word)
 
-    wc = if nchars(wc_new) == 5 * nwords(wc)
-            nwords(wc_new) > 2 && @info wc_new
-            wc_new
-         else
-            wc
-         end
-    # notice the type restriction on wordlist before judging my indexing
-    @inbounds for i in (idx+1):length(wordlist)
-        find_word_combo(wc, wordlist, i)
+function find_word_combo!(hits::Set{WordCombination}, wordlist::Vector{String})
+    wc = WordCombination()
+    p = Progress(length(wordlist); showspeed=true)
+    spinlock = Threads.SpinLock()
+    Threads.@threads for (i, a) in enumerate(wordlist)
+        skip(wc, a) && continue
+        wca = union(wc, a)
+        for (j, b) in enumerate(@view wordlist[i:end])
+            skip(wca, b) && continue
+            wcb = union(wca, b)
+            for (k, c) in enumerate(@view wordlist[j:end])
+                skip(wcb, c) && continue
+                wcc = union(wcb, a)
+                for (l, d) in enumerate(@view wordlist[k:end])
+                    skip(wcc, d) && continue
+                    wcd = union(wcc, d)
+                    for (m, e) in enumerate(@view wordlist[l:end])
+                        skip(wcd, e) && continue
+                        wce = union(wcd, e)
+                        # @info wce
+                        lock(spinlock)
+                        push!(hits, wce)
+                        unlock(spinlock)
+                    end
+                end
+            end
+        end
+        next!(p)
     end
-    return nothing
+
+    finish!(p)
+
+    return hits
 end
+
+# great way that results in a stack overflow
+# function find_word_combo!(hits::Set{WordCombination}, wc::WordCombination, wordlist::Vector{String}, idx::Int=1)
+#     # idx > 10 && return
+#     word = wordlist[idx]
+#     # word in wc && error("got a repeat! $(wc), $(idx)")
+
+#     chars = Set(word)
+#     # this depends on no words having duplicate letters
+#     wc = length(setdiff(chars, wc.chars)) == 5 ? wc : union(wc, word)
+#     # wc = length(setdiff(chars, wc.chars)) == 5 ? wc : union(wc, word)
+#     if nwords(wc) == 2
+#         @info wc
+#         # push!(hits, wc)
+#         return nothing
+#     end
+
+#     # notice the type restriction on wordlist before judging my indexing
+#     Threads.@threads for i in (idx+1):length(wordlist)
+#         find_word_combo!(hits, wc, wordlist, i)
+#     end
+#     return nothing
+# end
 
 # function find_word_combo(wc::WordCombination, wordlist::AbstractVector{String},
 #                          mask::BitVector=BitVector(true for _ in wordlist))
