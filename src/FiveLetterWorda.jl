a4e54b144075fe0e09650bdf9b0b223f3cc88ebf
Phillip Alday <me@phillipalday.com>
2022-08-05 21:38:20 -0500

    set API

--- src/FiveLetterWorda.jl
+++ src/FiveLetterWorda.jl
@@ -9,21 +9,52 @@ using Scratch
 using ZipFile
 
 export main, WordCombination, nwords, nchars
-export adjacency_matrix, cliques!
+export adjacency_matrix, cliques!, five_letter_words
 
 const CACHE = Ref("")
 const WORD_ZIP_URL = "https://github.com/dwyl/english-words/raw/master/words_alpha.zip"
 
 clear_scratchspaces!() = Scratch.clear_scratchspaces!(@__MODULE__)
 
-struct WordCombination
+"""
+    WordCombination
+    WordCombination()
+    WordCombination(w::String)
+    WordCombination(ws::AbstractVector{String})
+
+A conveniencce data type for a set of words and associated characters.
+
+Fields:
+- `chars::Set{Char}`
+- `words::Set{String}`
+
+Methods for [`union[!]`](@ref), [`intersect[!]`](@ref), [`push[!]`](@ref)
+and [`in[!]`](@ref) are provided.
+
+See also [`nwords`](@ref) and [`nchars`](@ref).
+"""
+struct WordCombination <: AbstractSet{String}
     chars::Set{Char}
     words::Set{String}
 end
 
+"""
+    nwords(wc::WordCombination)
+
+Return the number of words in a `wc`.
+
+See also [`nchars`](@ref).
+"""
 nwords(wc::WordCombination) = length(wc.words)
+
+"""
+    nchars(wc::WordCombination)
+
+Return the number of characters in a `wc`.
+
+See also [`nwords`](@ref).
+"""
 nchars(wc::WordCombination) = length(wc.chars)
-get_words(wc::WordCombination) = wc.words
 
 WordCombination() = WordCombination(Set{Char}(), Set{String}())
 WordCombination(w::String) = WordCombination(Set{Char}(w), Set{String}([w]))
@@ -37,16 +68,67 @@ function Base.show(io::IO, wc::WordCombination)
     return nothing
 end
 
-function WordCombination(ws::AbstractVector{String})
+function WordCombination(ws::Union{AbstractSet{String}, AbstractVector{String}})
     chars = mapreduce(Set, union!, ws; init=Set{Char}())
     return WordCombination(chars, Set{String}(ws))
 end
 
-# TODO intersect[!]
-# TODO push!
-function Base.union!(w1::WordCombination, w2::WordCombination)
-    union!(w1.chars, w2.chars)
-    union!(w1.words, w2.words)
+#####
+##### Set-like operations
+#####
+
+Base.length(wc::WordCombination) = nwords(wc)
+Base.in(w::String, wc::WordCombination) = w in wc.words
+Base.in(c::Char, wc::WordCombination) = c in wc.chars
+Base.isempty(wc::WordCombination) = isempty(wc.words)
+Base.iterate(wc::WordCombination, args...) = iterate(wc.words, args...)
+
+function Base.:(==)(w1::WordCombination, w2::WordCombination)
+    return w1.words == w2.words
+end
+
+function Base.empty!(wc::WordCombination)
+    empty!(wc.words)
+    empty!(wc.chars)
+    return wc
+end
+
+function Base.intersect(w1::WordCombination, w2::WordCombination)
+    words = intersect(w1.words, w2.words)
+    return WordCombination(words)
+end
+
+function Base.intersect!(w1::WordCombination, w2::WordCombination)
+    intersect!(w1.words, w2.words)
+    mapreduce(Set, intersect!, w2.words; init=w1.chars)
+    return w1
+end
+
+function Base.push!(wc::WordCombination, w::String)
+    union!(wc.chars, Set(w))
+    push!(wc.words, w)
+    return wc
+end
+
+function Base.setdiff(w1::WordCombination, w2::WordCombination)
+    words = setdiff(w1.words, w2.words)
+    return WordCombination(words)
+end
+
+function Base.setdiff!(w1::WordCombination, w2::WordCombination)
+    setdiff!(w1.words, w2.words)
+    mapreduce(Set, union!, w1.words; init=empty!(w1.chars))
+    return w1
+end
+
+function Base.symdiff(w1::WordCombination, w2::WordCombination)
+    words = symdiff(w1.words, w2.words)
+    return WordCombination(words)
+end
+
+function Base.symdiff!(w1::WordCombination, w2::WordCombination)
+    symdiff!(w1.words, w2.words)
+    mapreduce(Set, union!, w1.words; init=empty!(w1.chars))
     return w1
 end
 
@@ -56,7 +138,15 @@ function Base.union(w1::WordCombination, w2::WordCombination)
     return WordCombination(chars, words)
 end
 
-Base.in(w::String, wc::WordCombination) = w in wc.words
+function Base.union!(w1::WordCombination, w2::WordCombination)
+    union!(w1.chars, w2.chars)
+    union!(w1.words, w2.words)
+    return w1
+end
+
+#####
+##### Data loading
+#####
 
 function __init__()
     CACHE[] = @get_scratch!("data")
@@ -105,15 +195,32 @@ end
 
 remove_anagrams(words::Vector{String}) = unique(Set, words)
 
+"""
+    five_letter_words()
+
+Return the set of five-letter words containing five unique letters.
+
+Technically, this returns the equivlance classes, i.e., it removes
+all anagrams and leaves one word per anagram equivalence class.
+"""
+function five_letter_words()
+    words = remove_anagrams(load_data())
+    # remove words with repeated letters
+    words = filter(x -> length(Set(x)) == 5, words)
+    return words
+end
+
+#####
+##### The Algorithm™
+#####
+
 # TODO: expose constraint on word length
 # FIXME: do we really want to remove anagrams?
 # FIXME: if we are going to remove anagrams, should we select the
 #        representative of that equivalence class in some other way
 #        than "whatever was first in the file"?
 function main()
-    words = remove_anagrams(load_data())
-    # remove words with repeated letters
-    words = filter(x -> length(Set(x)) == 5, words)
+    words = five_letter_words()
     adj = adjacency_matrix(words)
     sets = Vector{Vector{String}}()
     cliques!(sets, adj, words)
@@ -125,6 +232,7 @@ function num_shared_neigbors(r1, r2, start=1)
         return x[1] & x[2]
     end
 end
+
 shared_neighbors(r1, r2, start=1) = @view(r1[start:end]) .& @view(r2[start:end])
 
 # TODO: turn this into a recursive call that allows find cliques of order n
